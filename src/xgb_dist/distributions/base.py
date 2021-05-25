73c6728394baa3897e1ca06a24404057419be1a9
Christian Donnerer <christian.donnerer@gmail.com>
2021-05-25 20:17:35 +0100

    cleaner repr for distributions

--- src/xgb_dist/distributions/base.py
+++ src/xgb_dist/distributions/base.py
@@ -4,6 +4,9 @@ from abc import ABC, abstractmethod
 
 
 class BaseDistribution(ABC):
+    def __repr__(self):
+        return self.__class__.__name__.lower()
+
     @abstractmethod
     def gradient_and_hessian(self, y, params):
         pass
