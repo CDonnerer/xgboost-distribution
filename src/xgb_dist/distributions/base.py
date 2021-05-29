425739caf8dbb0e9b24a642dc8027c6abfa72ba1
Christian Donnerer <christian.donnerer@gmail.com>
2021-05-29 12:25:05 +0100

    reverting ill-founded repr appraoch

--- src/xgb_dist/distributions/base.py
+++ src/xgb_dist/distributions/base.py
@@ -9,7 +9,7 @@ from abc import ABC, abstractmethod
 
 class BaseDistribution(ABC):
     def __repr__(self):
-        return f"`{self.__class__.__name__.lower()}`"
+        return self.__class__.__name__.lower()
 
     @property
     @abstractmethod
