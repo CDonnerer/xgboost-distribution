368bd222b6ca123856227fc1a9308bd2048b1dae
Christian Donnerer <christian.donnerer@gmail.com>
2021-06-08 22:04:38 +0100

    Perf comparison (#8)
    * fixed it with natural gradients
    
    * added performance comparison
    
    * updated readme with comp
    
    * rst syntax
    
    * rst syntax
    
    * rst syntax
    
    * cleanup

--- src/xgb_dist/model.py
+++ src/xgb_dist/model.py
@@ -13,8 +13,9 @@ from xgb_dist.distributions import get_distribution, get_distribution_doc
     extra_parameters=get_distribution_doc(),
 )
 class XGBDistribution(xgb.XGBModel):
-    def __init__(self, distribution=None, **kwargs):
+    def __init__(self, distribution=None, natural_gradient=True, **kwargs):
         self.distribution = distribution or "normal"
+        self.natural_gradient = natural_gradient
         super().__init__(objective=None, **kwargs)
 
     def fit(self, X, y, *, eval_set=None, early_stopping_rounds=None, verbose=True):
@@ -25,9 +26,10 @@ class XGBDistribution(xgb.XGBModel):
         params["disable_default_eval_metric"] = True
         params["num_class"] = len(self._distribution.params)
 
-        # TODO: setting base_score is crucial to get right as it's the starting
-        # point for all params of the distribution
-        params["base_score"] = np.mean(y)
+        # we set base score to zero to instead use base_margin in dmatrices
+        # this allows different starting values for the distribution params
+        params["base_score"] = 0.0
+        self._starting_params = self._distribution.starting_params(y)
 
         train_dmatrix, evals = _wrap_evaluation_matrices(
             missing=self.missing,
@@ -36,11 +38,12 @@ class XGBDistribution(xgb.XGBModel):
             group=None,
             qid=None,
             sample_weight=None,
-            base_margin=None,
+            base_margin=self._get_base_margins(len(y)),
             feature_weights=None,
             eval_set=eval_set,
             sample_weight_eval_set=None,
-            base_margin_eval_set=None,
+            # TODO: Clean up the hack
+            base_margin_eval_set=[self._get_base_margins(len(eval_set[0][1]))],
             eval_group=None,
             eval_qid=None,
             create_dmatrix=lambda **kwargs: xgb.DMatrix(nthread=self.n_jobs, **kwargs),
@@ -61,7 +64,11 @@ class XGBDistribution(xgb.XGBModel):
 
     def predict_dist(self, X):
         """Predict all params of the distribution"""
-        params = self._Booster.predict(xgb.DMatrix(X), output_margin=True)
+
+        params = self._Booster.predict(
+            xgb.DMatrix(X, base_margin=self._get_base_margins(X.shape[0])),
+            output_margin=True,
+        )
         return self._distribution.predict(params)
 
     def predict(self, X):
@@ -75,12 +82,10 @@ class XGBDistribution(xgb.XGBModel):
     def _objective_func(self):
         def obj(params: np.ndarray, data: xgb.DMatrix):
             y = data.get_label()
-            grad, hess = self._distribution.gradient_and_hessian(y, params)
-
-            flattened_len = len(y) * len(self._distribution.params)
-            grad = grad.reshape((flattened_len, 1))
-            hess = hess.reshape((flattened_len, 1))
-            return grad, hess
+            grad, hess = self._distribution.gradient_and_hessian(
+                y, params, self.natural_gradient
+            )
+            return grad.flatten(), hess.flatten()
 
         return obj
 
@@ -90,3 +95,12 @@ class XGBDistribution(xgb.XGBModel):
             return self._distribution.loss(y, params)
 
         return feval
+
+    def _get_base_margins(self, n_samples):
+        return (
+            np.array(
+                [param * np.ones(shape=(n_samples,)) for param in self._starting_params]
+            )
+            .transpose()
+            .flatten()
+        )
