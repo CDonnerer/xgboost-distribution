4ab2cc7289f4496f7e23662b3316ecde8b545960
Christian Donnerer <christian.donnerer@gmail.com>
2021-07-04 19:03:49 +0100

    using approximate fisher info

--- src/xgboost_distribution/distributions/negative_binomial.py
+++ src/xgboost_distribution/distributions/negative_binomial.py
@@ -30,17 +30,22 @@ class NegativeBinomial(BaseDistribution):
                         = p *( k - e^a * e^-b )
                         = p * ( k - n e^-b)
 
-    The Fisher Information = n / [ (1-p) p^2 ]
+    The Fisher Information:
+        I(n) ~ p / [ n*(p + 1) ]
+        I(p) = n / [ (1-p) p^2 ]
+
+    where we used the approximation shown here:
+    http://erepository.uonbi.ac.ke:8080/xmlui/handle/123456789/33803
 
     In reparameterized form:
 
-        n / [ (1-p) p^2 ] = I ( d/dn log(n) )^2 = I ( 1/n )^2
+        p / [ n*(p + 1) ] = I ( d/dn log(n) )^2 = I ( 1/n )^2
         n / [ (1-p) p^2 ] = I ( d/dp log(p/(1-p)) )^2 = I ( 1/(p-p^2) )^2
 
     We find:
 
-        [  n**3 / [ (1-p) p^2 ], 0  ]
-        [  0,  n / p ]
+        [  np / (p+1), 0  ]
+        [  0,    n / p    ]
 
     Ref:
         https://www.wolframalpha.com/input/?i=d%2Fda+-log%28+%5B1+%2F+%281+%2B+e%5E%28-b%29%29%5D+%5E%28e%5Ea%29+%281+-+%5B1+%2F+%281+%2B+e%5E%28-b%29%29%5D%29%5Ek+binomial%28%28e%5Ea%29+%2B+k+-+1%2C+%28e%5Ea%29+-+1%29+%29
@@ -68,7 +73,7 @@ class NegativeBinomial(BaseDistribution):
 
             fisher_matrix = np.zeros(shape=(len(y), 2, 2))
             fisher_matrix[:, 0, 0] = n / (p * (1 - p) ** 2)
-            fisher_matrix[:, 1, 1] = 1 / (n * p)
+            fisher_matrix[:, 1, 1] = (n * p) / (p + 1)
 
             grad = np.linalg.solve(fisher_matrix, grad)
             hess = np.ones(shape=(len(y), 2))  # we set the hessian constant
