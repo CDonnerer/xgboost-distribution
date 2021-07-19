5eb9c480b32bf7d1766b610bec9cf55da107d458
Christian Donnerer <christian.donnerer@gmail.com>
2021-07-19 20:42:18 +0100

    another commit for good measure

--- examples/benchmarking.py
+++ examples/benchmarking.py
@@ -1,126 +1,133 @@
+import glob
+import logging
 import os
+import shutil
+import sys
+import tempfile
 import time
+import zipfile
 from argparse import ArgumentParser
 from dataclasses import dataclass
+from functools import partial
+from pathlib import Path
 
 import numpy as np
 import pandas as pd
 import requests
 from ngboost import NGBRegressor
-from ngboost.learners import default_linear_learner, default_tree_learner
 from scipy.stats import norm
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import KFold, train_test_split
+from xgboost import XGBRegressor
 
 from xgboost_distribution import XGBDistribution
 
-np.random.seed(1)
+_logger = logging.getLogger(__name__)
+
+
+# -----------------------------------------------------------------------------
+# Datasets for benchmarking
+# -----------------------------------------------------------------------------
+
+DATASETS = {}  # each instantiated dataset will be stored here, keyed by name
+DATA_DIR = Path(__file__).parent.parent.absolute().joinpath("data")
 
 
 @dataclass
 class Dataset:
-    # name: str
+    name: str
     url: str
-    load_func: callable
-    load_kwargs: dict
+    unpack: str = None
+    load_func: callable = pd.read_csv
+    processing_func: callable = None
+
+    def __post_init__(self):
+        DATASETS.update({self.name: self})
 
 
-housing = Dataset(
-    url=(
-        "https://archive.ics.uci.edu/ml/machine-learning-databases/"
-        "housing/housing.data"
-    ),
-    load_func=pd.read_csv,
-    load_kwargs={"header": None, "delim_whitespace": True},
+Dataset(
+    name="housing",
+    url="https://archive.ics.uci.edu/ml/machine-learning-databases/housing/housing.data",  # noqa: E501
+    load_func=partial(pd.read_csv, header=None, delim_whitespace=True),
 )
 
-wine = Dataset(
-    url=(
-        "https://archive.ics.uci.edu/ml/machine-learning-databases/"
-        "wine-quality/winequality-red.csv"
-    ),
-    load_func=pd.read_csv,
-    load_kwargs={"delimiter": ";"},
+Dataset(
+    name="wine",
+    url="https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv",  # noqa: E501
+    load_func=partial(pd.read_csv, delimiter=";"),
 )
 
-yacht = Dataset(
-    url=(
-        "http://archive.ics.uci.edu/ml/machine-learning-databases/00243/"
-        "yacht_hydrodynamics.data"
-    ),
-    load_func=pd.read_csv,
-    load_kwargs={"header": None, "delim_whitespace": True},
+Dataset(
+    name="naval",
+    url="https://archive.ics.uci.edu/ml/machine-learning-databases/00316/UCI%20CBM%20Dataset.zip",  # noqa: E501
+    unpack="data.txt",
+    load_func=partial(pd.read_csv, header=None, delim_whitespace=True),
+    processing_func=lambda x: x.iloc[:, :-1],
 )
 
-datasets = {"housing": housing, "wine": wine, "yacht": yacht}
+Dataset(
+    name="energy",
+    url="https://archive.ics.uci.edu/ml/machine-learning-databases/00242/ENB2012_data.xlsx",  # noqa: E501
+    load_func=pd.read_excel,
+    processing_func=lambda x: x.iloc[:, :-1],
+)
+
+Dataset(
+    name="yacht",
+    url="http://archive.ics.uci.edu/ml/machine-learning-databases/00243/yacht_hydrodynamics.data",  # noqa: E501
+    load_func=partial(pd.read_csv, header=None, delim_whitespace=True),
+)
+
+Dataset(
+    name="concrete",
+    url="https://archive.ics.uci.edu/ml/machine-learning-databases/concrete/compressive/Concrete_Data.xls",  # noqa: E501
+    load_func=pd.read_excel,
+)
 
 
-def load_dataset(name):
-    dataset = datasets[name]
-    local_path = f"data/{name}.data"
+def load_dataset(name, data_dir=DATA_DIR):
+    dataset = DATASETS[name]
+
+    os.makedirs(data_dir, exist_ok=True)
+    local_path = f"{DATA_DIR}/{name}.data"
 
     if os.path.exists(local_path):
-        return dataset.load_func(local_path, **dataset.load_kwargs)
+        df = dataset.load_func(local_path)
+        if dataset.processing_func:
+            df = dataset.processing_func(df)
+        return df.iloc[:, :-1].values, df.iloc[:, -1].values
     else:
-        print("Dataset not locally cached, downloading from url...")
+        _logger.info("Dataset not locally cached, downloading from url...")
         r = requests.get(dataset.url)
-        with open(local_path, "wb") as f:
-            f.write(r.content)
-        print(f"Downloaded file to {local_path}")
+
+        if dataset.unpack is not None:
+            with tempfile.TemporaryFile() as fp:
+                fp.write(r.content)
+                unpack_file_from_zip(fp, dataset.unpack, local_path)
+        else:
+            with open(local_path, "wb") as f:
+                f.write(r.content)
+
+        _logger.info(f"Downloaded file to {local_path}")
         return load_dataset(name)
 
 
-dataset_name_to_loader = {
-    "housing": lambda: pd.read_csv(
-        (
-            "https://archive.ics.uci.edu/ml/machine-learning-databases/"
-            "housing/housing.data"
-        ),
-        header=None,
-        delim_whitespace=True,
-    ),
-    "concrete": lambda: pd.read_excel(
-        (
-            "https://archive.ics.uci.edu/ml/machine-learning-databases/"
-            "concrete/compressive/Concrete_Data.xls"
-        )
-    ),
-    "wine": lambda: pd.read_csv(
-        (
-            "https://archive.ics.uci.edu/ml/machine-learning-databases/"
-            "wine-quality/winequality-red.csv"
-        ),
-        delimiter=";",
-    ),
-    "kin8nm": lambda: pd.read_csv("data/uci/kin8nm.csv"),
-    "naval": lambda: pd.read_csv(
-        "data/naval/data.txt", delim_whitespace=True, header=None
-    ).iloc[:, :-1],
-    "power": lambda: pd.read_excel("data/uci/power-plant.xlsx"),
-    "energy": lambda: pd.read_excel(
-        (
-            "https://archive.ics.uci.edu/ml/machine-learning-databases/"
-            "00242/ENB2012_data.xlsx"
-        )
-    ).iloc[:, :-1],
-    "protein": lambda: pd.read_csv("data/uci/protein.csv")[
-        ["F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "RMSD"]
-    ],
-    "yacht": lambda: pd.read_csv(
-        (
-            "http://archive.ics.uci.edu/ml/machine-learning-databases/00243/"
-            "yacht_hydrodynamics.data"
-        ),
-        header=None,
-        delim_whitespace=True,
-    ),
-    "msd": lambda: pd.read_csv("data/uci/YearPredictionMSD.txt").iloc[:, ::-1],
-}
+def unpack_file_from_zip(zip_file, to_unpack, path):
+    with zipfile.ZipFile(zip_file, "r") as inzipfile:
+        with tempfile.TemporaryDirectory() as temp_dir:
+            inzipfile.extractall(path=temp_dir)
+            data_file = glob.glob(f"{temp_dir}/**/{to_unpack}", recursive=True)[0]
+            shutil.copyfile(data_file, path)
 
-base_name_to_learner = {
-    "tree": default_tree_learner,
-    "linear": default_linear_learner,
-}
+
+# dataset_name_to_loader = {
+#     "kin8nm": lambda: pd.read_csv("data/uci/kin8nm.csv"),
+#     "power": lambda: pd.read_excel("data/uci/power-plant.xlsx"),
+#     "protein": lambda: pd.read_csv("data/uci/protein.csv")[
+#         ["F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "RMSD"]
+#     ],
+#     "msd": lambda: pd.read_csv("data/uci/YearPredictionMSD.txt").iloc[:, ::-1],
+# }
 
 
 @dataclass
@@ -133,41 +140,53 @@ class SplitData:
     y_test: np.ndarray
 
 
-def rmse(preds, y_test):
-    return np.sqrt(mean_squared_error(preds.loc, y_test))
+def rmse(y_pred, y_test):
+    return np.sqrt(mean_squared_error(y_pred, y_test))
 
 
-def nll(preds, y_test):
-    return -norm.logpdf(y_test.flatten(), loc=preds.loc, scale=preds.scale).mean()
+def normal_nll(loc, scale, y_test):
+    return -norm.logpdf(y_test.flatten(), loc=loc, scale=scale).mean()
 
 
 @dataclass
 class EvalResults:
+    name: str
     total_time: float
     rmse: float
     nll: float
 
 
 def summarize_results(results):
-    records = [(result.rmse, result.nll, result.total_time) for result in results]
-    df = pd.DataFrame.from_records(records, columns=["rmse", "nll", "total_time"])
+    # records = [(result.rmse, result.nll, result.total_time) for result in results]
+    df = pd.DataFrame(results)
+    # df = df.set_index("name")
+    # records, columns=["rmse", "nll", "total_time"])
     return df.agg(["mean", "std"], axis=0)
 
 
-def fit_predict_ngb(data):
+evaluations = []
 
-    start_time = time.time()
 
+def evaluate(evaluation_func):
+    def measured(data):
+        t0 = time.perf_counter()
+        preds = evaluation_func(data)
+        elapsed = time.perf_counter() - t0
+        return EvalResults(
+            name=evaluation_func.__name__,
+            rmse=rmse(preds.loc, data.y_test),
+            nll=normal_nll(preds.loc, preds.scale, data.y_test),
+            total_time=elapsed,
+        )
+
+    evaluations.append(measured)
+    # evaluate.update({evaluation_func.__name__: measured})
+    return measured
+
+
+@evaluate
+def ngb_regressor(data):
     ngb = NGBRegressor(verbose=False)
-    # Base=base_name_to_learner[args.base],
-    # Dist=eval(args.distn),
-    # Score=eval(args.score),
-    # n_estimators=args.n_est,
-    # learning_rate=args.lr,
-    # natural_gradient=args.natural,
-    # minibatch_frac=args.minibatch_frac,
-    # verbose=args.verbose,
-
     ngb.fit(
         data.X_train,
         data.y_train,
@@ -175,20 +194,12 @@ def fit_predict_ngb(data):
         Y_val=data.y_val,
         early_stopping_rounds=10,
     )
-    preds = ngb.pred_dist(data.X_test, max_iter=ngb.best_val_loss_itr)
-
-    elapsed_time = time.time() - start_time
-    return EvalResults(
-        rmse=rmse(preds, data.y_test),
-        nll=nll(preds, data.y_test),
-        total_time=elapsed_time,
-    )
+    return ngb.pred_dist(data.X_test, max_iter=ngb.best_val_loss_itr)
 
 
-def fit_predict_xgbd(data):
-    start_time = time.time()
-
-    xgbd = XGBDistribution(n_estimators=500)
+@evaluate
+def xgb_distribution(data):
+    xgbd = XGBDistribution(max_depth=3, natural_gradient=True, n_estimators=500)
     xgbd.fit(
         data.X_train,
         data.y_train,
@@ -196,71 +207,59 @@ def fit_predict_xgbd(data):
         early_stopping_rounds=10,
         verbose=False,
     )
-    preds = xgbd.predict(data.X_test)
+    return xgbd.predict(data.X_test)
 
-    elapsed_time = time.time() - start_time
-    return EvalResults(
-        rmse=rmse(preds, data.y_test),
-        nll=nll(preds, data.y_test),
-        total_time=elapsed_time,
+
+# @evaluate
+def xgb_regressor(data):
+    xgb = XGBRegressor(max_depth=3, n_estimators=500)
+    xgb.fit(
+        data.X_train,
+        data.y_train,
+        eval_set=[(data.X_test, data.y_test)],
+        early_stopping_rounds=10,
+        verbose=False,
+    )
+    return xgb.predict(data.X_test)
+
+
+def setup_logging(loglevel=logging.INFO):
+    logformat = "[%(asctime)s] %(levelname)s:%(name)s:%(message)s"
+    logging.basicConfig(
+        level=loglevel, stream=sys.stdout, format=logformat, datefmt="%Y-%m-%d %H:%M:%S"
     )
 
 
-if __name__ == "__main__":
-
+def parse_args():
     argparser = ArgumentParser()
     argparser.add_argument("--dataset", type=str, default="concrete")
-    argparser.add_argument("--reps", type=int, default=5)
-    argparser.add_argument("--n-est", type=int, default=2000)
-    argparser.add_argument("--n-splits", type=int, default=10)
-    argparser.add_argument("--distn", type=str, default="Normal")
-    argparser.add_argument("--lr", type=float, default=0.01)
-    argparser.add_argument("--natural", action="store_true")
-    argparser.add_argument("--score", type=str, default="MLE")
-    argparser.add_argument("--base", type=str, default="tree")
-    argparser.add_argument("--minibatch-frac", type=float, default=None)
-    argparser.add_argument("--verbose", action="store_true")
-    args = argparser.parse_args()
+    argparser.add_argument("--random-seed", type=int, default=42)
+    argparser.add_argument("--n-folds", type=int, default=10)
+    return argparser.parse_args()
 
-    # load dataset -- use last column as label
-    data = load_dataset(args.dataset)
-    X, y = data.iloc[:, :-1].values, data.iloc[:, -1].values
 
-    if not args.minibatch_frac:
-        args.minibatch_frac = 1.0
+if __name__ == "__main__":
+    args = parse_args()
+    setup_logging()
 
-    print(f"Dataset={args.dataset} X.shape={X.shape}")
+    np.random.seed(args.random_seed)
 
-    if args.dataset == "msd":
-        folds = [(np.arange(463715), np.arange(463715, len(X)))]
-        args.minibatch_frac = 0.1
-    else:
-        kf = KFold(n_splits=args.n_splits)
-        folds = kf.split(X)
+    X, y = load_dataset(args.dataset)
+    _logger.info(f"Loaded dataset: `{args.dataset}`, X.shape={X.shape}")
 
-        n = X.shape[0]
-        np.random.seed(1)
-        folds = []
-        for i in range(args.n_splits):
-            permutation = np.random.choice(range(n), n, replace=False)
-            end_train = round(n * 9.0 / 10)
-            end_test = n
+    kf = KFold(n_splits=args.n_folds, shuffle=True, random_state=args.random_seed)
+    _logger.info(f"Cross-validation with {args.n_folds} folds...")
 
-            train_index = permutation[0:end_train]
-            test_index = permutation[end_train:n]
-            folds.append((train_index, test_index))
+    results = [list() for eval in evaluations]
 
-    ngb_res, xgbd_res = list(), list()
-
-    for itr, (train_index, test_index) in enumerate(folds):
-        print(f"Fold {itr+1}")
+    for ii, (train_index, test_index) in enumerate(kf.split(X)):
+        _logger.info(f"Fold {ii+1} / {args.n_folds}...")
 
         X_train, X_test = X[train_index], X[test_index]
         y_train, y_test = y[train_index], y[test_index]
         X_train, X_val, y_train, y_val = train_test_split(
-            X_train, y_train, test_size=0.2
+            X_train, y_train, test_size=0.1, random_state=args.random_seed
         )
-
         split_data = SplitData(
             X_train=X_train,
             y_train=y_train,
@@ -269,9 +268,8 @@ if __name__ == "__main__":
             X_test=X_test,
             y_test=y_test,
         )
+        for eval_func, result in zip(evaluations, results):
+            result.append(eval_func(split_data))
 
-        ngb_res.append(fit_predict_ngb(split_data))
-        xgbd_res.append(fit_predict_xgbd(split_data))
-
-    print(summarize_results(ngb_res))
-    print(summarize_results(xgbd_res))
+    for result in results:
+        _logger.info(f"{result[0].name}\n{summarize_results(result)}")
