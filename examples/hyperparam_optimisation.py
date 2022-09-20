6eff33dbd4c58e6fd52b9b8d9a49a966ddf7532d
Christian Donnerer <christian.donnerer@gmail.com>
2022-09-20 22:26:43 +0100

    feeling a bit stupid right now

--- examples/hyperparam_optimisation.py
+++ examples/hyperparam_optimisation.py
@@ -1,6 +1,6 @@
 """Example for hyperparameter optimsation
 """
-from scipy.stats import norm
+import scipy
 from sklearn.datasets import load_boston
 from sklearn.metrics import make_scorer
 from sklearn.model_selection import GridSearchCV
@@ -8,9 +8,18 @@ from sklearn.model_selection import GridSearchCV
 from xgboost_distribution import XGBDistribution
 
 
-def normal_distribution_log_loss_score(y, y_pred):
-    loc, scale = y_pred.loc, y_pred.scale
-    return norm.logpdf(y, loc=loc, scale=scale).mean()
+def ll_score_func(distribution):
+    # TODO: Why isn't this working?
+    dists = {"normal": scipy.stats.norm}
+
+    def score_func(y, y_pred):
+        return dists[distribution](y, *y_pred).mean()
+
+    return score_func
+
+
+def normal_ll_score(y, y_pred):
+    return scipy.stats.norm.logpdf(y, *y_pred).mean()
 
 
 def main():
@@ -18,13 +27,16 @@ def main():
 
     X, y = data.data, data.target
 
-    param_grid = {"n_estimators": [10, 20, 50, 100], "max_depth": [1, 3, 6]}
+    param_grid = {"n_estimators": [1, 5, 10, 20], "max_depth": [1, 2, 3]}
+
+    # normal_ll_score = ll_score_func("normal")
+    # breakpoint()
 
     xgb_cv = GridSearchCV(
         XGBDistribution(),
         param_grid,
-        cv=3,
-        scoring=make_scorer(normal_distribution_log_loss_score),
+        cv=5,
+        scoring=make_scorer(normal_ll_score),
     )
 
     xgb_cv.fit(X, y)
