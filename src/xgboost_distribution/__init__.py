2c0e547e84ca121fdb370b4996fb9c24236f972c
Christian Donnerer <christian.donnerer@gmail.com>
2022-10-20 22:16:17 +0100

    Versioning cleanup (#73)
    * no more python 3.7 support
    
    * helpful hint

--- src/xgboost_distribution/__init__.py
+++ src/xgboost_distribution/__init__.py
@@ -1,14 +1,7 @@
-import sys
-
-if sys.version_info[:2] >= (3, 8):
-    # TODO: Import directly (no need for conditional) when `python_requires = >= 3.8`
-    from importlib.metadata import PackageNotFoundError, version  # pragma: no cover
-else:
-    from importlib_metadata import PackageNotFoundError, version  # pragma: no cover
+from importlib.metadata import PackageNotFoundError, version  # pragma: no cover
 
 try:
-    # Change here if project is renamed and does not equal the package name
-    dist_name = __name__
+    dist_name = __name__  # assuming project and package name are equal
     __version__ = version(dist_name)
 except PackageNotFoundError:  # pragma: no cover
     __version__ = "unknown"
