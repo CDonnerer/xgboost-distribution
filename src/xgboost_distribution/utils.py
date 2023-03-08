e51d395c2c25e08ee088de3cba8f646d5106ec59
Christian Donnerer <christian.donnerer@gmail.com>
2023-03-08 16:51:18 +0000

    easier than expected

--- src/xgboost_distribution/utils.py
+++ src/xgboost_distribution/utils.py
@@ -3,8 +3,8 @@ from contextlib import contextmanager
 
 @contextmanager
 def hide_attributes(cls, args):
-    stashed = {arg: cls.__dict__.pop(arg) for arg in args}
+    stash = {arg: cls.__dict__.pop(arg) for arg in args}
     try:
         yield
     finally:
-        cls.__dict__.update(stashed)
+        cls.__dict__.update(stash)
