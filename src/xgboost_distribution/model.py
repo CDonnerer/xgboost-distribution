c17a23414a917af0ffe066ca05b92b8dd1aed5ce
Christian Donnerer <christian.donnerer@gmail.com>
2025-02-15 16:37:03 +0000

    [Fix] sklearn >= 1.6 compat (#105)
    * adding tags
    
    * what are these tags actually used for
    
    * fix examples
    
    * maybe inherit directly from XGBRegressor?
    
    * update changelog

--- src/xgboost_distribution/model.py
+++ src/xgboost_distribution/model.py
@@ -3,16 +3,29 @@
 import importlib
 import json
 import os
-from typing import Any, Callable, List, Optional, Sequence, Tuple, Union, no_type_check
+from typing import (
+    Any,
+    Callable,
+    List,
+    Optional,
+    Sequence,
+    Tuple,
+    Union,
+    no_type_check,
+)
 
 import numpy as np
-from sklearn.base import RegressorMixin
 from sklearn.utils.validation import check_is_fitted
 from xgboost._typing import ArrayLike
 from xgboost.callback import TrainingCallback
 from xgboost.config import config_context
 from xgboost.core import Booster, DMatrix, _deprecate_positional_args
-from xgboost.sklearn import XGBModel, _wrap_evaluation_matrices, xgboost_model_doc
+from xgboost.sklearn import (
+    XGBModel,
+    XGBRegressor,
+    _wrap_evaluation_matrices,
+    xgboost_model_doc,
+)
 from xgboost.training import train
 
 from xgboost_distribution.distributions import get_distribution, get_distribution_doc
@@ -27,7 +40,7 @@ from xgboost_distribution.utils import to_serializable
     natural_gradient : bool, default=True
         Whether or not natural gradients should be used.""",
 )
-class XGBDistribution(XGBModel, RegressorMixin):
+class XGBDistribution(XGBRegressor):
     @_deprecate_positional_args
     def __init__(
         self,
